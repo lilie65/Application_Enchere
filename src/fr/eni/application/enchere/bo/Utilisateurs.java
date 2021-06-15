bb10f9a15b8ca74c7cc983a3c7cc03d66fea95d0
lilie65 <celine.kerdoud@gmail.com>
2021-06-15 11:22:41 +0200

    Ajout de la couche bo pour l'utilisateur

---
+++ src/fr/eni/application/enchere/bo/Utilisateurs.java
@@ -0,0 +1,103 @@
+package fr.eni.application.enchere.bo;
+
+public class Utilisateurs {
+ private String pseudo;
+ private String nom;
+ private String prenom;
+ private String email;
+ private int telephone;
+ private String rue;
+ private int codePostal;
+ private String ville;
+ private String motDePasse;
+ private int credit;
+ public Utilisateurs(String pseudo, String nom, String prenom, String email, int telephone, String rue, int codePostal,
+			String ville, String motDePasse, int credit, boolean admnistrateur) {
+		super();
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.motDePasse = motDePasse;
+		this.credit = credit;
+		this.admnistrateur = admnistrateur;
+	}
+	private boolean admnistrateur;
+ public String getPseudo() {
+	return pseudo;
+}
+public void setPseudo(String pseudo) {
+	this.pseudo = pseudo;
+}
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public String getPrenom() {
+	return prenom;
+}
+public void setPrenom(String prenom) {
+	this.prenom = prenom;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public int getTelephone() {
+	return telephone;
+}
+public void setTelephone(int telephone) {
+	this.telephone = telephone;
+}
+public String getRue() {
+	return rue;
+}
+public void setRue(String rue) {
+	this.rue = rue;
+}
+public int getCodePostal() {
+	return codePostal;
+}
+public void setCodePostal(int codePostal) {
+	this.codePostal = codePostal;
+}
+public String getVille() {
+	return ville;
+}
+public void setVille(String ville) {
+	this.ville = ville;
+}
+public String getMotDePasse() {
+	return motDePasse;
+}
+public void setMotDePasse(String motDePasse) {
+	this.motDePasse = motDePasse;
+}
+public int getCredit() {
+	return credit;
+}
+public void setCredit(int credit) {
+	this.credit = credit;
+}
+public boolean isAdmnistrateur() {
+	return admnistrateur;
+}
+public void setAdmnistrateur(boolean admnistrateur) {
+	this.admnistrateur = admnistrateur;
+}
+
+@Override
+	public String toString() {
+		return "pseudo "+pseudo+ " nom "+nom+" prenom "+ prenom+ " email"+email +" telephone "+ telephone + " rue "+rue+
+				" codePostal "+ codePostal+ "ville "+ville + "mot de passe "+ motDePasse+ "crédit "+ credit;
+				
+	}
+}
